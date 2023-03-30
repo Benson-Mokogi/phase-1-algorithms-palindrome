e9ce949cfca2d338a2842b41558846b025700405
Benson Mokogi <b.mokogi@gmail.com>
2023-03-30 17:17:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 function isPalindrome(word) {
+  const le = word.length;
+  
+  if (le == 0 || le == 1){
+    console.log(true)
+    return true;
+  } else if (word.length > 1){
+    
+    for (i=0; i<=le/2; i++){
+      if (word[i] !== word[le-1-i]){
+
+        console.log(false)
+        return false;
+      }
+    }
+    
+    console.log(true)
+    return true;
+  }
   // Write your algorithm here
 }
-
+isPalindrome("madam")
 /* 
   Add your pseudocode here
+  1. check if the input is an empty string or one letter and return true
+
+  2. if the string has more than two characters, 
+     compare the last and first, second and second last and so on recursively.
+
+  3. If the characters are strickly equal for upto 1/2 the length of the string during recursion,
+      return true, else return false.  
 */
 
 /*
